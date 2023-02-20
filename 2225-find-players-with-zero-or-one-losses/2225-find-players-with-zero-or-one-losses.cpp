c2eaf5e16ba6811f39171a4c50243fb606d902c0
조현창 <gusckd57@naver.com>
2023-02-20 13:48:18 +0900

    Time: 822 ms (42.36%), Space: 162.5 MB (89.52%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         vector<vector<int>> answer(2,vector<int>());
-        map<int,int> cnt;        
+        map<int,int> cnt;         
         for(auto& m : matches){
             if(cnt.find(m[0]) == cnt.end())cnt[m[0]] = 0;
             if(cnt.find(m[1]) == cnt.end())cnt[m[1]] = 0;
             cnt[m[1]] += 1;
         }
         for(auto& c:cnt){
-            cout<<c.second<<" "<<c.first<<endl;
             if(c.second == 0)answer[0].push_back(c.first);
             if(c.second == 1)answer[1].push_back(c.first);
         }
-        sort(answer[0].begin(), answer[0].end());
-        sort(answer[1].begin(), answer[1].end());
         return answer;
         
     }
